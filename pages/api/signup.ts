153922898346c481416fb5c89627c81c1fcdb02e
Conan McNicholl <conan.mcnicholl@songtradr.com>
2022-05-16 23:14:28 +0100

    Authentication - 2

--- pages/api/signup.ts
+++ pages/api/signup.ts
@@ -5,5 +5,43 @@ import jwt from "jsonwebtoken";
 import prisma from "../../lib/prisma";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  prisma;
+  const salt = bcrypt.genSaltSync();
+  const { email, password } = req.body;
+
+  let user;
+
+  try {
+    user = await prisma.user.create({
+      data: {
+        email,
+        password: bcrypt.hashSync(password, salt),
+      },
+    });
+  } catch (error) {
+    res.status(401);
+    res.json({ error: "This user already exists" });
+  }
+
+  const token = jwt.sign(
+    {
+      email: user.email,
+      id: user.id,
+      time: Date.now(),
+    },
+    ">>>secretKey<<<",
+    { expiresIn: "8h" }
+  );
+
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("fullstack_access_token", token, {
+      httpOnly: true,
+      maxAge: 8 * 60 * 60,
+      path: "/",
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
+    })
+  );
+
+  res.json(user);
 };
