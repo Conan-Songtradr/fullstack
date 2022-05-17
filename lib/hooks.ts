2a320da79f51ecf432438c5e15f2c2ead05d1c74
Conan McNicholl <conan.mcnicholl@songtradr.com>
2022-05-17 23:15:02 +0100

    Fetching Data - 1

---
+++ lib/hooks.ts
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+import useFetch from "./useFetch";
+
+useSWR;
+useFetch;
+
+export const useUser = () => {
+  const { data, error, isValidating } = useSWR("/user", useFetch);
+
+  return {
+    user: data,
+    isLoading: isValidating,
+    isError: error,
+  };
+};
+
+export const usePlaylists = () => {
+  const { data, error, isValidating } = useSWR("/playlists", useFetch);
+
+  return {
+    playlists: data || [],
+    isLoading: isValidating,
+    isError: error,
+  };
+};
